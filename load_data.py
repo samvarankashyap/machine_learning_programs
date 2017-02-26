4ca39ce8e4374fb480e3fcab64a2cd4c7094132f
samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2017-02-25 20:03:23 -0500

    Added data files in data folder Added script to load data into numpy array

---
+++ load_data.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+dataset = pd.read_csv('Data.csv')
+# all the columns except the last column from data.csv 
+X = dataset.iloc[:, :-1].values
+# choose the the last column of data.csv
+Y = dataset.iloc[:, 3].values
