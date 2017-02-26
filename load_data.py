10094e148496ad28a66e984485a1b4fdc5d460aa
samvaran kashyap rallabandi <samvaran.kashyap@gmail.com>
2017-02-25 20:09:31 -0500

    bugfix: fixed path for Data.csv

--- load_data.py
+++ load_data.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv('Data.csv')
+dataset = pd.read_csv('data/Data.csv')
 # all the columns except the last column from data.csv 
 X = dataset.iloc[:, :-1].values
 # choose the the last column of data.csv
